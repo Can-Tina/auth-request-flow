ce0802f8d4351e58fb7e1a5b344a2df8adf42240
Can-Tina <lennihart@gmail.com>
2022-02-14 16:03:31 +0000

    Complete

--- src/router.js
+++ src/router.js
@@ -14,11 +14,26 @@ const mockUser = {
 };
 
 router.post('/login', (req, res) => {
+    const {
+        username,
+        password
+    } = req.body
 
+    if(username === mockUser.username && password === mockUser.password) {
+        const token = jwt.sign({ username: username, password: password }, 'secretkeywow')
+        res.json(token)
+    }
+    res.json("Login Failed. Invalid Credentials")
 });
 
 router.get('/profile', (req, res) => {
-
+    let decoded = ""
+    try {
+        decoded = jwt.verify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImF1dGhndXkiLCJpYXQiOjE2NDQ4NTMxODJ9.zZNb6Wl5mNZ6keaZvte5ptgTjiCCvCiWEr4wQY-Ymno', 'secretkeywow');
+    } catch(err) {
+        res.json("Mission Failed")
+    }
+    res.json(decoded)
 });
 
 module.exports = router;
